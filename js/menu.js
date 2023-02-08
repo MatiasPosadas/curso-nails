6dde5b114e24c64e3a8b371cb48f0a4c0f8e2fa5
Matias Posadas <93284124+MatiasPosadas@users.noreply.github.com>
2023-02-08 19:53:11 -0300

    Retoques
    Se 'retoco'
    
    El menu desplegable del hero.
    Se le cambio el color, se agrando tipografia, se reparo el cierre al elegir una opcion.
    
    Se acomodo al medio el nav del footer.

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,7 @@
     const openButton = document.querySelector('.nav__menu');
     const menu = document.querySelector('.nav__link');
     const closeMenu = document.querySelector('.nav__close');
+    const closeMenu2= document.querySelector('.nav__link');
 
     openButton.addEventListener('click', ()=>{
         menu.classList.add('nav__link--show');
@@ -10,5 +11,10 @@
 
     closeMenu.addEventListener('click', ()=>{
         menu.classList.remove('nav__link--show');
-    })
+    });
+
+    closeMenu2.addEventListener('click', ()=>{
+        menu.classList.remove('nav__link--show');
+    });
+
 })();
