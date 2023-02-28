b7431b4299f8de564f4ce94974e083778f2a55d3
Matias Posadas <93284124+MatiasPosadas@users.noreply.github.com>
2023-02-28 03:40:38 -0300

    Se agrego REVEAL SCROLL

---
+++ js/index.js
@@ -0,0 +1,31 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.nav__logo,.nav__link,.hero__container', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.reveal-left', {
+        duration: 3000,
+        origin: 'left',
+        distance: '200px'
+    });
+
+    sr.reveal('.reveal-right', {
+        duration: 3000,
+        origin: 'right',
+        distance: '300px'
+    });
+
+    sr.reveal('.reveal-down', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.reveal-top', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-200px'
+    });
